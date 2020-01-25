468720be95ac77e206cbac88a16587f629c2f75a
Homson15 <42740619+Homson15@users.noreply.github.com>
2020-01-25 18:54:49 +0100

    adding comments

--- js/display.js
+++ js/display.js
@@ -1,7 +1,10 @@
 
 //	Variables
 
-var border = {
+var copyArray = [];
+var booleanCopy = false;
+
+var border = { //Zmienne guzików
 	
 	Camera: document.getElementById('bCamera'),
 	
@@ -9,55 +12,180 @@ var border = {
 	
 	Vars: document.getElementById('bVars'),
 	
-	ProgrammingStuff: document.getElementById('bProgrammingStuff'),
+	Autonomus: document.getElementById('bAutonomus'),
+	
+}
+
+var canvas ={ //zapisywanie drogi do poszczególnych wskaźników
+
+	width: 300,
+	
+	driveLMotor: document.getElementById('driveLMotorIndicator').getContext("2d"),
+	
+	//lMotorValue: parseFloat(document.getElementById('lMotorValue').value),
+	
+	driveRMotor: document.getElementById('driveRMotorIndicator').getContext("2d"),
+	
+	//rMotorValue: parseFloat(document.getElementById('rMotorValue').value),
+	
+	intakeMotor: document.getElementById('intakeMotorIndicator').getContext("2d"),
+	
+	//intakeMotorValue: parseFloat(document.getElementById('intakeMotorValue').value),
+	
+	shootLMotor: document.getElementById('shootLMotorIndicator').getContext("2d"),
+	
+	shootRMotor: document.getElementById('shootRMotorIndicator').getContext("2d"),
 	
 }
 
 //	JQ
 
 $(document).ready(function(){
-  changeHeight();
-  showdiv(1);
+  changeHeight();			//Środkowanie wyskokości
+  canvasTranslate();		//Ustawienie canvasów na środku
+  showdiv(1);				//pokazanie pierwszego diva po odpaleniu strony
   $("#Stats").slideUp(0);
   $("#Vars").slideUp(0);
-  $("#ProgrammingStuff").slideUp(0);
+  $("#Autonomus").slideUp(0); //animacja przechodzenia między kartami
   $("#bCamera").click(function(){
 	$("#Stats").slideUp(350);
 	$("#Vars").slideUp(300);
-	$("#ProgrammingStuff").slideUp(300);
+	$("#Autonomus").slideUp(300);
     $("#Camera").delay(300).slideDown(300);
 	showdiv(1);
   });
   $("#bStats").click(function(){
     $("#Camera").slideUp(300);
 	$("#Vars").slideUp(300);
-	$("#ProgrammingStuff").slideUp(300);
+	$("#Autonomus").slideUp(300);
 	$("#Stats").delay(300).slideDown(300);
 	showdiv(2);
   });
   $("#bVars").click(function(){
     $("#Camera").slideUp(300);
 	$("#Stats").slideUp(300);
-	$("#ProgrammingStuff").slideUp(300);
+	$("#Autonomus").slideUp(300);
 	$("#Vars").delay(300).slideDown(300);
 	showdiv(3);
   });
-  $("#bProgrammingStuff").click(function(){
+  $("#bAutonomus").click(function(){
     $("#Camera").slideUp(300);
 	$("#Stats").slideUp(300);
 	$("#Vars").slideUp(300);
-	$("#ProgrammingStuff").delay(300).slideDown(300);
+	$("#Autonomus").delay(300).slideDown(300);
 	showdiv(4);
   });
 });
 
 //	Functions
 
-function showdiv(a){
+function onLoad(){
+	
+	//chart(canvas.driveLMotor, 300); //Przykładowe wartości do wskaźników
+	//chart(canvas.driveRMotor, -300);
+	chart(canvas.intakeMotor, 135);
+	chart(canvas.shootLMotor, 270);
+	chart(canvas.shootRMotor, 260);
+}
+
+function canvasTranslate(){			//Transfer położenia punktu (0.0) na środek wskaźnika
+	
+	canvas.driveLMotor.translate(400,3); // Canvas ma długosć 400px, ale używane set jedynie 300px
+	canvas.driveRMotor.translate(400,3);
+	canvas.intakeMotor.translate(400,3);
+	canvas.shootLMotor.translate(400,3);
+	canvas.shootRMotor.translate(400,3);
+	
+}
+
+function chart(ctx, x){ //Rysowanie wartości wskaźników
+	
+	if(x > canvas.width) x = canvas.width;			//sprawdzenie czy wartość nie wychodzi poza wartosć (gdyby mapowanie poszło źle)
+	
+	ctx.fillStyle = "#404040"						//dopasowanie tła wskaźnika
+	ctx.fillRect(-canvas.width,0,canvas.width*2,30)	//narysowanie tła, głównie w celu odświerzenia przed ponownym narysowaniem
+	
+	var grd = ctx.createRadialGradient(0,15,1,0,15,canvas.width*1);	//przejście z jednego koloru na drugie (x1, y1, r1, x2, y2, r2) - Fukncja ta wyznacza 2 koła z przejściem w kolorach między nimi
+	grd.addColorStop(0,'#49879F');					//Kolor 1
+	grd.addColorStop(1,'#78179b');					//Kolor 2
+	
+	ctx.fillStyle = grd;							//Nadanie prostokątowi (wskaźnikowi) kolorów przejścia
+	ctx.fillRect(0,0,x,30);							//Narysowanie wskaźnika
+	
+	ctx.lineWidth = 3;									//Grubość linii 
+	ctx.strokeRect(-canvas.width,0,canvas.width*2,30)	//Obramówka do wskaźnika
+	
+}
+
+function copy(elem){
+	
+	elem.setAttribute('onclick',''); // Wyłączenie funkcji, aby nie robić paru takich samych okien
+	
+	var box = document.createElement('div');
+	var div1 = document.createElement('div');
+	var div2 = document.createElement('div');
+	var nr = elem.id.slice(7);					//pobieranie miejsca zmiennej
+	var span = document.createElement('span');
+	
+	
+	document.getElementById('field').appendChild(box);	//Wsadzenie 'Pudła' na kopie do miejsca na kopie
+
+	box.style.border = "2px solid #202020"				
+	box.className = "box"
+	box.id = "box" + nr;
+	box.style.height = "102px"							
+	box.style.position = "absolute";					//Ważny element, aby div mógł się poruszać
+	box.style.textAlign = "center";
+	
+	
+	div1.innerHTML = elem.innerHTML;					//Pobranie nazwy zmiennej
+	div1.className = elem.className;					//Pobranie klasy, aby nowy pojemnik był podobny
+	div1.id = "header" + nr;
+	div1.style.borderBottom = "2px solid #202020"
+	div1.style.backgroundColor = "#303030"
+	div1.style.margin = "0px";
+	div1.style.paddingLeft = "0px";
+	
+	div1.style.width = "180px";
+	
+	box.appendChild(div1);								//Wsadzenie do pudła
+	
+	div2.className = "var";
+	div2.style.display = "block"
+	
+	box.appendChild(div2);
+	
+	span.id = "copy" + nr;								//nadanie tekstowi id, aby go później zaktualizować w listener()
+	span.innerHTML = document.getElementById('var'+nr).value;	//	
+	
+	div2.appendChild(span);
+	
+	copyArray.push(nr); //Wstawienie danej zmiennej do tablicy, aby odwołać się do niej w każdej pętli Networtable
+	
+	booleanCopy = true; //Zwolnienie blokady, aby nie było błędów
+	
+	dragElement(document.getElementById('box'+nr));
+	
+}
+
+function listener(){
+	
+	if(booleanCopy){
+		
+		for (let i = 0; i < copyArray.length; i++) {
+			//update zmiennych w kopiach
+			document.getElementById('copy' + copyArray[i]).innerHTML = document.getElementById('var' + copyArray[i]).value;
+			
+		}
+	}
+	
+}
+
+function showdiv(a){ //Zmiana border-bottom, aby ładnie wyglądało
 	
 	hidediv();
 	
-	switch(a){
+	switch(a){ //Ukrycie danego border-bottom
 		
 		case 1:
 			border.Camera.style.borderBottomColor = "#404040";
@@ -69,24 +197,69 @@ function showdiv(a){
 			border.Vars.style.borderBottomColor = "#404040";
 			break;
 		case 4:
-			border.ProgrammingStuff.style.borderBottomColor = "#404040";
+			border.Autonomus.style.borderBottomColor = "#404040";
 			break;
 	}
 	
 }
 
-function hidediv(){
+function hidediv(){ //Pokazanie każdego border-bottom
 	
 	border.Camera.style.borderBottomColor = "#202020";
 	border.Stats.style.borderBottomColor = "#202020";
 	border.Vars.style.borderBottomColor = "#202020";
-	border.ProgrammingStuff.style.borderBottomColor = "#202020";
+	border.Autonomus.style.borderBottomColor = "#202020";
 	
 }
 
-function changeHeight(){
+function changeHeight(){ //Wyśrdkowanie względem wysokości
 	
 	var height = window.innerHeight;
 	document.body.style.marginTop =  (height - 755)/2 + "px";
 	
+}
+
+function dragElement(elmnt) {							//Sprawianie aby dany elemeny mógł być poruszany
+	
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	
+	var nr = elmnt.id.slice(3);	
+	
+	if (document.getElementById("header" + nr)) {
+		// if present, the header is where you move the DIV from:
+		document.getElementById("header" + nr).onmousedown = dragMouseDown;
+	} else {
+		// otherwise, move the DIV from anywhere inside the DIV:
+		elmnt.onmousedown = dragMouseDown;
+	}
+	
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
+	
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// set the element's new position:
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement() {
+		// stop moving when mouse button is released:
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
 }
--- js/ui.js
+++ js/ui.js
@@ -1,56 +1,55 @@
 // Define UI elements
-var oldGyro = 0;
-var gyro = 0;
-var oldArm = 0;
-var newArm = 0;
+
+var i=0;
 
 var ui = {
-    timer: document.getElementById('timer'),
 	
-	rasberryState: document.getElementById('rasp-status'),
+	robotState: document.getElementById('robotStatus'),
 	
-    robotState: document.getElementById('robotstatus'),
+	timer: document.getElementById('timer'),
 	
-	armposition: document.getElementById('point'),
+	stats: {
 	
-	robotGyro: document.getElementById('gyro-arm'),
-	
-	gyroValue: document.getElementById('gyro-value'),
-	
-	varList: document.getElementById('Vars'),
-	
-	autonomusList: document.getElementById('AutoSelect'),
-	
-	cameraContainer: document.getElementById('Camera'),
-	
-	robotArm: document.getElementById('robo-arm'),
-	
-	tapeDetected: document.getElementById('rasbIfSee'),
-	
-	tapeYaw: document.getElementById('textyaw'),
-	
-	elevator: {
-		low: document.getElementById('lvl1'),
-		middle: document.getElementById('lvl2'),
-		high: document.getElementById('lvl3')
+		driveLMotor: document.getElementById('driveLMotorIndicator').getContext("2d"),
+		
+		driveLMotorValue: document.getElementById('lMotorValue'),
+		
+		driveRMotor: document.getElementById('driveRMotorIndicator').getContext("2d"),
+		
+		driveRMotorValue: document.getElementById('rMotorValue'),
+		
+		intakeMotor: document.getElementById('intakeMotorIndicator').getContext("2d"),
+		
+		intakeMotorValue: document.getElementById('intakeMotorValue'),
+		
+		shootLMotor: document.getElementById('shootLMotorIndicator').getContext("2d"),
+		
+		intakeMotorValue: document.getElementById('shootLMotorIndicator'),
+		
+		shootRMotor: document.getElementById('shootRMotorIndicator').getContext("2d"),
+		
+		intakeMotorValue: document.getElementById('shootRMotorIndicator'),
 	},
 	
+	varList: document.getElementById('tuning'),
+	
+	camera: {
+		target: document.getElementById('kamera'),
+	}
 };
 
 // Key Listeners
 
 NetworkTables.addRobotConnectionListener(onRobotConnection, true);
 
-function A(){
-	
-		ui.robotGyro.style.transform = "rotate(60deg)";
-	
-}
+
 
 function onRobotConnection(connected) {
 	var state = connected ? 'Connected!' : 'Disconnected';
 	console.log(state);
-	ui.robotState.innerHTML = state;
+	
+	if(connected) ui.robotState.style.background = "green";
+	else ui.robotState.style.background = "red";
 }
 
 //attachSelectToSendableChooser("autonomusList", "Autonomous Mode");
@@ -58,19 +57,19 @@ function onRobotConnection(connected) {
 
 NetworkTables.addGlobalListener(onValueChanged, true);
 
-
+/*
 //This button is just an example of triggering an event on the robot by clicking a button.
-// NetworkTables.addKeyListener('/SmartDashboard/value1', (key,value) => {
-	//function addData(chart1," " , value);
-	//addData(chart1," ",Math.floor(Math.random() * 90)+1);
-	// ui.testdiv1.innerHTML = value;
+NetworkTables.addKeyListener('/SmartDashboard/value1', (key,value) => {
+	function addData(chart1," " , value);
+	addData(chart1," ",Math.floor(Math.random() * 90)+1);
+	ui.testdiv1.innerHTML = value;
 	
-// });
+});
 
-// NetworkTables.addKeyListener('/SmartDashboard/gyro', (key,value) => {
-	// ui.robotGyro.style.transform = "rotate(" + value + "deg)";
+NetworkTables.addKeyListener('/SmartDashboard/gyro', (key,value) => {
+	ui.robotGyro.style.transform = "rotate(" + value + "deg)";
 	
-// });
+ */
 
 function onValueChanged(key,value,isNew) {
 	
@@ -81,198 +80,127 @@ function onValueChanged(key,value,isNew) {
 	}
 	
 	switch(key) {
-		case '/SmartDashboard/TapeDetected':
-			if(value){ 
-				ui.tapeDetected.style.fill = 'green';
+		case '/SmartDashboard/tv':	//Pobieranie 'Czy robot widzi target'
+		
+			if(value == 1){
+				ui.camera.target.style.borderColor = 'green';
 			}else {
-				ui.tapeDetected.style.fill = 'red';
-			};
+				ui.camera.target.style.borderColor = 'red';
+			} break;
+		
+		case '/SmartDashboard/timer': //Pobierane timera
 			
-		case '/SmartDashboard/Gyro':
-			//ui.testdiv1.innerHTML = value;
-			var visibleAngle = Math.round(value);
-			if(value > 360) {
-				visibleAngle = visibleAngle - 360;
-			}else if( value < 0) {
-				VisiblaAnagale = visibleAngle + 360;
-			}
-			//var visibleAngle = Math.round(value);
-			ui.gyroValue.innerHTML = visibleAngle;
-			ui.robotGyro.style.transform = "rotate(" + -value + "deg)";
-			console.log("gyro: " + gyro);
-			break;
-		// case '/SmartDashboard/gyro':
-			//ui.testdiv1.innerHTML = value;
-			// ui.gyroValue.innerHTML = value;
-			// gyro = value - oldGyro;
-			// oldGyro = value;
-			// ui.robotGyro.style.transform = "rotate(" + gyro + "deg)";
-			
-			// console.log("gyro: " + gyro);
-			// break;
-		case '/SmartDashboard/ElevatorOutput':
-			canvas1 = value;
-			updateCanvas();
-			//changeFirstCanvas(value);
-			break;
-		case '/SmartDashboard/Average':
-			canvas2 = value;
-			updateCanvas();
-			//changeSecondCanvas(value * 0.75);
-			break;
-		case '/SmartDashboard/IntakeRawOutput':
-		case '/SmartDashboard/ClimbingSpeed':
-			canvas3 = value;
-			updateCanvas()
-			//changeThirdCanvas(value * 0.75);
-			break;
-		case '/SmartDashboard/ClimbingSpeed':
-		case '/SmartDashboard/IntakeRawOutput':
-			canvas4 = value;
-			updateCanvas()
-			//changeFourthCanvas(value);
-			break;
-		case '/SmartDashboard/ElevatorEncoder': //Trzeba dorobić w kodzie robota
-			newArm = value - oldArm;
-			oldArm = value;
-			ui.robotArm.style.transform = ('translate(0px,' + newArm * -13000 + 'px)');
-			console.log(newArm);
-			
-			if(value > 0.16 && value < 0.2) {
-				ui.elevator.low.style.fill = 'green'
-			}else {
-				ui.elevator.low.style.fill = 'red'
-			}
-			if(value > 0.44 && value < 0.48) {
-				ui.elevator.middle.style.fill = 'green'
-			}else {
-				ui.elevator.middle.style.fill = 'red'
-			}
-			if(value > 0.63 && value < 0.67) {
-				ui.elevator.high.style.fill = 'green'
-			}else {
-				ui.elevator.high.style.fill = 'red'
-			}
-			break;
-		case '/SmartDashboard/TapeYaw':
-			if(value){
-				ui.tapeYaw.innerHTML = value;
-			}
-		case '/SmartDashboard/timer':
 			ui.timer.innerHTML = parseInt(value) < 0 ? '0:00' : Math.floor(parseInt(value) / 60) + ':' + (parseInt(value) % 60 < 10 ? '0' : '') + parseInt(value) % 60;
+			if(value<30) ui.timer.style.color = "red";
+			else ui.timer.style.color = "white";
 			break;
+		
+		case '/SmartDashboard/LeftEncoder': //Pobierane timera
+			
+			drivLMotorValue = value;
+			chart(driveLMotor,value*300);
+			break;
+			
+		case '/SmartDashboard/LeftEncoderV': //Pobierane timera
+			
+			drivRMotorValue = value;
+			chart(driveRMotor,value*300);
+			break;
+			
 	}
 	
 	
-	//początek  /CameraPublisher/
-	
 	if (isNew && !document.getElementsByName(key)[0]) {
-		// Make a new div for this value
-		if(key.substring(0,17) === "/CameraPublisher/") {
-				ui.rasberryState.innerHTML = "Connected";
-			console.log("rasperka działa");
-		} else if(key.substring(0,16) != "/SmartDashboard/") {
-			console.log("raspberka wysyla syf");
+		// Jeżeli 'nowa wartość' + nie jest pusta
+		if(key.substring(0,11) === "/limelight/") { //sprawdzanie czy limelight wysyła dane = limelight podłączony
+			ui.camera.limelight = "10px solid red"
+		} else if(key.substring(0,16) != "/SmartDashboard/" /*|| key.substring(0,25) == "/SmartDashboard/limelight"*/) {
+			//filtrowanie śmieci
+			console.log("limelight wysyła dane");
 		} else {
+			
 			key = key.slice(16);
-			var div = document.createElement('div'); // Make div
-		ui.varList.appendChild(div); // Add the div to the page
-		
-		var p = document.createElement('p'); // Make a <p> to display the name of the property
-		p.innerHTML = key; // Make content of <p> have the name of the NetworkTables value
-		div.appendChild(p); // Put <p> in div
-
-		var input = document.createElement('input'); // Create input
-		input.name = key; // Make its name property be the name of the NetworkTables value
-		input.value = value; // Set
-		// The following statement figures out which data type the variable is.
-		// If it's a boolean, it will make the input be a checkbox. If it's a number,
-		// it will make it a number chooser with up and down arrows in the box. Otherwise, it will make it a textbox.
-		if (value === true || value === false) { // Is it a boolean value?
-			input.type = 'checkbox';
-			input.checked = value; // value property doesn't work on checkboxes, we'll need to use the checked property instead
-		} else if (!isNaN(value)) { // Is the value not not a number? Great!
-			input.type = 'number';
-			div.style.background = "#323232";
-		} else { // Just use a text if there's no better manipulation method
-			input.type = 'text';
-		}
-		
-		div.style.width = "400px";
-		div.style.height= "60px";
-		
-		// Create listener for value of input being modified
-		input.onchange = function() {
-			switch (input.type) { // Figure out how to pass data based on input type
-				case 'checkbox':
-					// For booleans, send bool of whether or not checkbox is checked
-					NetworkTables.putValue(key, input.checked);
-					break;
-				case 'number':
-					// For number values, send value of input as an int.
-					NetworkTables.putValue(key, parseInt(input.value));
-					break;
-				case 'text':
-					// For normal text values, just send the value.
-					NetworkTables.putValue(key, input.value);
-					break;
+			
+			var divBox = document.createElement('div');	//Tworzenie 'Pudła'
+			divBox.name = "box"+ i;						
+			divBox.className = "box"; 					
+			ui.varList.appendChild(divBox);				//Wsadzenie go do listy zmiennych
+			
+			
+			var div1 = document.createElement('div');	//Tworzenie diva na nazwę zmiennej
+			divBox.appendChild(div1);					//Wsadzenie go do 'Pudła'
+			
+			div1.className = "element";					//Nadanie mu własności
+			div1.setAttribute("id", "element" + i);		//Nadanie ID, aby go potem znaleźć
+			div1.setAttribute('onclick','copy(this)');	//Nadanie funkcji 'onclick' 
+			
+			var p = document.createElement('span');		
+			p.innerHTML = key;							//Pobranie nazwy zmiennej
+			div1.appendChild(p); 						//Wsadzenie jej do pierwszego diva
+			
+			var div2 = document.createElement('div');	//Stworzenie drugiego diva na wartości
+			div2.className = "var";						
+			
+			divBox.appendChild(div2);					//Wsadzenie go do 'Pudła'
+			
+			var input = document.createElement('input');//Stworzenie inputu
+			input.name = key;							//Nadanie mu nazwy, aby go potem znaleźć i zaktualizować
+			input.value = value;						//Pobranie warości zmiennej
+			
+			//Ta sekcja zmienia input do jego typu dantych (boolean/number), jeżeli żaden z nich, to 'text'
+			//Pomijam ze względu na dalsze obliczenia z .value
+			
+			/*//if (value === true || value === false) {// Sprawdzenie czy to boolean
+				// input.type = 'checkbox'; 			// Zamiana inputu na checkox
+				// input.checked = value; 				// .value nie działa na checkbox, potrzeba urzyć własności checkboxów
+			// } else */if (!isNaN(value)) {			// Sprawdzanie czy nie jest nie liczbą 
+				input.type = 'number';					// Zmiana imputu na liczbę
+			} else {
+				input.type = 'text';					// Najlepsza opcja dla bliżej nie znanego typu danych oraz samego tekstu
 			}
-		 };
-		// Put the input into the div.
-		div.appendChild(input);
+			
+			input.style.width = "180px";				// Nadanie inputowi własności div2
+			input.style.height = "37px"
+			input.id = "var" + i;						// Nadanie ID aby odnaleźć wartość
+			
+			// listener na wprowadzenie zmiennych do robota
+			
+			// input.onchange = function() {
+				// switch (input.type) { // Figure out how to pass data based on input type
+					// case 'checkbox':
+						// Dla checkboxów, sprawdza czy jest zaznaczony i wysyła wynik true/false
+						// NetworkTables.putValue(key, input.checked);
+						// break;
+					// case 'number':
+						// Oddaje wartość w zmiennej int
+						// NetworkTables.putValue(key, parseInt(input.value));
+						// break;
+					// case 'text':
+						// Dla tekstu po prostu wysyła wartość
+						// NetworkTables.putValue(key, input.value);
+						// break;
+				// }
+			 // };
+			 
+			div2.appendChild(input);					// Wsadzenie imputu do divu na zmienne
+			i++;
 		}
-	} else { // If the value is not new
-		// Find already-existing input for changing this variable
-		key = key.slice(16);
-		var oldInput = document.getElementsByName(key)[0];
-		if (oldInput) { // If there is one (there should be, unless something is wrong)
-			if (oldInput.type === 'checkbox') { // Figure out what data type it is and update it in the list
+	} else {											// Jeżeli wartość już istnieje 
+		
+		key = key.slice(16);							// Ucięcie "/SmartDashboard/"
+		var oldInput = document.getElementsByName(key)[0]; // Szukanie elementu po nazwie
+		if (oldInput) { 								// Powinno byc zawsze true, chyba że coś pójdzie nie tak
+			if (oldInput.type === 'checkbox') { 		// Sprawdza typ danych i go aktualizuje
 				oldInput.checked = value;
 			} else {
 				oldInput.value = value;
 			}
 		} else {
 			//console.log('Error: Non-new variable ' + key + ' not present in tuning list!');
-			}
-			
 		}
-		
-	};
-	//koniec
-
-//gyro-arm
-// NetworkTables.addKeyListener('/SmartDashboard/value1', (key,value) => {
-	// ui.testdiv.innerHTML = value;
-// });
-
-// NetworkTables.addKeyListener('/SmartDashboard/', (key,value) => {
-	// ui.rioRAMuse.innerHTML = value;
-// });
-
-// NetworkTables.addKeyListener('/SmartDashboard/RPRed', (key, value) => {
-    // ui.RPRed.innerHTML = value;
-// });
-
-// NetworkTables.addKeyListener('/SmartDashboard/RPBlue', (key, value) => {
-    // ui.RPBlue.innerHTML = value;
-//});
-
-
-// NetworkTables.addKeyListener(''), (key, value) => {
-	// ui.gyro.val = value;
-    // ui.gyro.visualVal = Math.floor(ui.gyro.val - ui.gyro.offset);
-    // ui.gyro.visualVal %= 360;
-    // if (ui.gyro.visualVal < 0) {
-        // ui.gyro.visualVal += 360;
-    // }
-    // ui.gyro.arm.style.transform = `rotate(${ui.gyro.visualVal}deg)`;
-    // ui.gyro.number.innerHTML = ui.gyro.visualVal + 'ยบ';
-// });
-
-// NetworkTables.addKeyListener('/SmartDashboard/timer', (key, value) => {
-    //This is an example of how a dashboard could display the remaining time in a match.
-    //We assume here that value is an integer representing the number of seconds left.
-    // ui.timer.innerHTML = parseInt(value) < 0 ? '0:00' : Math.floor(parseInt(value) / 60) + ':' + (parseInt(value) % 60 < 10 ? '0' : '') + parseInt(value) % 60;
-// });
-
-
+	}
+	
+	
+	listener();											// Dodatkowy listener aby aktualizować resztę zmiennych
+	
+};
